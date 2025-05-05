3eb92abcb16b325746863bd1ad3393e41d7f3f3b
MedLhnin <mohamedbelfaquih1@gmail.com>
2025-05-05 09:33:29 +0000

    application.properties --> application.*.properties to differentiate between dev and prod environment

--- src/main/java/com/example/demo/JOBS/ScheduledInvoiceJob.java
+++ src/main/java/com/example/demo/JOBS/ScheduledInvoiceJob.java
@@ -43,7 +43,6 @@ public class ScheduledInvoiceJob {
                 template.setLastGenerated(LocalDateTime.now());
                 scheduledInvoiceRepository.save(template);
 
-                // invoiceService.SendEmailAndChangeInvoiceStatus(invoice);
             }
         }
     }
