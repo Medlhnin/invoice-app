011e0df625a7d039e4a9620c31d351ff63309afa
MohamedBelfaquih <lahnin.010@gmail.com>
2025-05-08 14:38:44 +0000

    mentionned InvoicePaymentRepository

--- src/main/java/com/example/demo/REPOSITORIES/InvoicePaymentRepository.java
+++ src/main/java/com/example/demo/REPOSITORIES/InvoicePaymentRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.REPOSITORIES;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvoicePaymentRepository extends JpaRepository<InvoicePaymentRepository, Long> {
 
 }
